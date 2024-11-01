5ac5a8d7a464be0e6f65713989fe96da7be7cd54
jimao <shihao.gu@perfma.com>
2024-11-02 00:42:30 +0800

    server配置

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    open: true,
+    host: '0.0.0.0',
+  },
   plugins: [react()],
 })
